5253973afe97d584f20d3a3144977c7fd77a8643
arksw <arksw@protonmail.com>
2025-06-08 15:22:24 +0200

    fix VA_COUNT macro for 0 args using non standard extension __VA_OPT__

--- core_preface.h
+++ core_preface.h
@@ -20,8 +20,9 @@
 #define SW_TOK_CAT2(a,b) SW__TOK_CAT2(a,b)
 #define SW__TOK_STR(a) #a
 #define SW_TOK_STR(a) SW__TOK_STR(a)
+// @FIXME: ideally figure out a way for VA_COUNT to return 0 if no args are provided!
 #define SW__VA_COUNT(a1,a2,a3,a4,a5,a6,a7,a8,a9,a10,a11,a12,a13,a14,a15,a16,count,...) count
-#define SW_VA_COUNT(...) SW__VA_COUNT(__VA_ARGS__,16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
+#define SW_VA_COUNT(...) SW__VA_COUNT(__VA_ARGS__ __VA_OPT__(,) 16,15,14,13,12,11,10,9,8,7,6,5,4,3,2,1,0)
 #define SW_ARR_COUNT(arr) (sizeof(arr)/sizeof((arr)[0]))
 #define SW_OFFSET_OF(T,member) ((SwUsz)((SwU8*)&((T*)0)->member - (SwU8*)0))
 
